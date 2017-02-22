ae473c45a6d092d6c67947a941d5eea90c6da8a5
Benjamin Duignan <benjaminduignan@Benjamins-MacBook-Air.local>
2017-02-21 21:35:49 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+// app.use is how you register middleware
+app.use(express.static(__dirname + '/public'));
+
+// next exists so you can tell express when your middleware function is done
+// if you dont put something in for next, then the handlers will never finish
+app.use((request, response, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${request.method} ${request.url}`
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    };
+  });
+  next();
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+  hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+  })
+
+app.get('/', (request, response) => {
+  response.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to the home page'
+  })
+});
+
+app.get('/about', (request, response) => {
+  response.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (request, response) => {
+  response.send({
+    errorMessage: 'Error!'
+  })
+});
+
+app.listen(3000);
